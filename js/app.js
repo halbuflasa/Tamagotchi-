b4d4679fc6036f3355f618ba67514c9d6dcd4f49
halbuflasa <albuflasahessa@gmail.com>
2024-10-05 19:27:00 +0300

    Tamagotchi game

--- js/app.js
+++ js/app.js
@@ -26,7 +26,7 @@ let gameOver;
 /*------------------------ Cached Element References ------------------------*/
 const  boredomStatEl = document.querySelector('#boredom-stat');
 const  hungerStatEl = document.querySelector('#hunger-stat');
-const  sleepinessStatEl = document.querySelector('sleepiness-stat');
+const  sleepinessStatEl = document.querySelector('#sleepiness-stat');
 
 const playBtnEl = document.querySelector('#play');
 const feedBtnEl = document.querySelector('#feed');
@@ -34,25 +34,67 @@ const sleepBtnEl = document.querySelector('#sleep');
 
 const gameMessageEl = document.querySelector('#message');
 const resetBtnEl = document.querySelector('#restart');
+
 /*-------------------------------- Functions --------------------------------*/
  function init (){
-    gameOver = false;
-    setInterval(runGame,2000);
-    render();
+   gameMessageEl.classList.add('hidden');
+   resetBtnEl.classList.add('hidden');
+   state.boredom = 0;
+   state.hunger= 0;
+   state.sleepiness= 0;
+   gameOver = false;
+   setInterval(runGame,2000);
+   render();
  }
  function runGame(){
-    console.log( 'the game is running!');
+   updateStates();
+   checkGameOver();
+   render();
+ 
  }
  function render(){
+   boredomStatEl.textContent= state.boredom;
+   hungerStatEl.textContent= state.hunger;
+   sleepinessStatEl.textContent= state.sleepiness;
 
+   if(gameOver){
+      clearInterval(timer);
+      gameMessageEl.classList.remove('hidden');
+      resetBtnEl.classList.remove('hidden');
+   }
  }
  function updateStates(){
-    const incremeter = Math.floor(Math.random() * 4)
-    state.boredom+=incremeter;
-    state.hunger+= incremeter
-    state.sleepiness+=incremeter;
+    state.boredom+=Math.floor(Math.random() * 4);
+    state.hunger+= Math.floor(Math.random() * 4);
+    state.sleepiness+=Math.floor(Math.random() * 4);
+ }
+
+ function checkGameOver(){
+    if (state.boredom >= 10 || state.hunger >= 10 || state.sleepiness >= 10) {
+      gameOver = true;
+    }
+     
+ }
+ function playBtnClick(){
+   state.boredom = 0;
+   render();
+ }
+
+ function feedBtnClick(){
+   state.hunger= 0;
+   render();
+ }
+ function sleepBtnClick(){
+   state.sleepiness= 0;
+   render();
  }
  init();
 
-/*----------------------------- Event Listeners -----------------------------*/
 
+
+/*----------------------------- Event Listeners -----------------------------*/
+document.querySelector('#play').addEventListener('click', playBtnClick);
+document.querySelector('#feed').addEventListener('click', feedBtnClick);
+document.querySelector('#sleep').addEventListener('click', sleepBtnClick);
+
+document.querySelector('#restart').addEventListener('click',init);
